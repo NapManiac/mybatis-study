9342e84345462aa0cae79bd01afa15db0c6aa7ed
nap <1394327107@qq.com>
2020-06-19 23:38:27 +0800

    复习mybatis

---
+++ mybatis-01/src/main/java/com/fly/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.fly.mapper;
+
+import com.fly.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    public List<User> selectUser();
+}
---
+++ mybatis-01/src/main/java/com/fly/pojo/User.java
@@ -0,0 +1,49 @@
+package com.fly.pojo;
+
+public class User {
+    private int id;  //id
+    private String name;   //姓名
+    private String pwd;   //密码
+
+    public User() {
+    }
+
+    public User(int id, String name, String pwd) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
